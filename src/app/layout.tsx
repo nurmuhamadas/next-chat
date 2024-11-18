817a52d9a82a655825df79769825c9a043283c66
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-11-19 06:28:12 +0700

    Style config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
-import localFont from "next/font/local"
+import { Open_Sans } from "next/font/google"
 
 import type { Metadata } from "next"
 
 import "./globals.css"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--open-sans",
 })
 
 export const metadata: Metadata = {
@@ -27,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${openSans.variable} antialiased`}>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,78 @@
 import type { Config } from "tailwindcss"
 import plugin from "tailwindcss/plugin"
+import tailwindAnimate from "tailwindcss-animate"
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          hover: "hsl(var(--primary-hover))",
+        },
+        accent: "hsl(var(--accent))",
+        surface: "hsl(var(--surface))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        grey: {
+          1: "hsl(var(--grey-1))",
+          2: "hsl(var(--grey-2))",
+          3: "hsl(var(--grey-3))",
+          4: "hsl(var(--grey-4))",
+        },
+        error: "hsl(var(--error))",
+        success: "hsl(var(--success))",
+        warning: "hsl(var(--warning))",
+        info: "hsl(var(--info))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        "open-sans": ["var(--open-sans)"],
+      },
+      fontSize: {
+        h1: ["34px", { fontWeight: "700", lineHeight: "42px" }],
+        h2: ["24px", { fontWeight: "700", lineHeight: "36px" }],
+        h3: ["20px", { fontWeight: "500", lineHeight: "28px" }],
+        h4: ["18px", { fontWeight: "500", lineHeight: "20px" }],
+        h5: ["16px", { fontWeight: "600", lineHeight: "24px" }],
+        "subtitle-1": ["16px", { fontWeight: "500", lineHeight: "24px" }],
+        "subtitle-2": ["14px", { fontWeight: "600", lineHeight: "20px" }],
+        "body-1": ["16px", { fontWeight: "400", lineHeight: "24px" }],
+        "body-2": ["14px", { fontWeight: "400", lineHeight: "20px" }],
+        button: ["14px", { fontWeight: "500", lineHeight: "20px" }],
+        caption: ["12px", { fontWeight: "400", lineHeight: "16px" }],
+        overline: ["10px", { fontWeight: "400", lineHeight: "14px" }],
+      },
+    },
   },
   plugins: [
     plugin(function ({ addUtilities }) {
@@ -79,6 +94,6 @@ export default {
         },
       })
     }),
-      require("tailwindcss-animate")
-],
+    tailwindAnimate,
+  ],
 } satisfies Config
