4a2077daddc0c49b2d913d837483cbe781ea8691
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-11-19 08:57:40 +0700

    Component: Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,79 @@
+import { PlusIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-h1">Heading 1</h1>
-      <h2 className="text-h2">Heading 2</h2>
-      <h3 className="text-h3">Heading 3</h3>
-      <h4 className="text-h4">Heading 4</h4>
-      <h5 className="text-h5">Heading 5</h5>
-      <div className="text-subtitle-1">Subtitle 1</div>
-      <div className="text-subtitle-2">Subtitle 2</div>
-      <div className="text-body-1">Body 1</div>
-      <div className="text-body-2">Body 2</div>
-      <div className="text-button">Button</div>
-      <div className="text-caption">Caption</div>
-      <div className="text-overline">Overline</div>
+    <div className="flex flex-col gap-y-8">
+      <div className="flex gap-x-4">
+        <Button size="icon">
+          <PlusIcon />
+        </Button>
+        <Button size="sm">Button Primary</Button>
+        <Button>Button Primary</Button>
+        <Button size="lg">Button Primary</Button>
+        <Button size="xl">Button Primary</Button>
+      </div>
+      <div className="flex gap-x-4">
+        <Button variant="secondary" size="icon">
+          <PlusIcon />
+        </Button>
+        <Button variant="secondary" size="sm">
+          Button Primary
+        </Button>
+        <Button variant="secondary">Button Primary</Button>
+        <Button variant="secondary" size="lg">
+          Button Primary
+        </Button>
+        <Button variant="secondary" size="xl">
+          Button Primary
+        </Button>
+      </div>
+      <div className="flex gap-x-4">
+        <Button variant="destructive" size="icon">
+          <PlusIcon />
+        </Button>
+        <Button variant="destructive" size="sm">
+          Button Primary
+        </Button>
+        <Button variant="destructive">Button Primary</Button>
+        <Button variant="destructive" size="lg">
+          Button Primary
+        </Button>
+        <Button variant="destructive" size="xl">
+          Button Primary
+        </Button>
+      </div>
+      <div className="flex gap-x-4">
+        <Button variant="outline" size="icon">
+          <PlusIcon />
+        </Button>
+        <Button variant="outline" size="sm">
+          Button Primary
+        </Button>
+        <Button variant="outline">Button Primary</Button>
+        <Button variant="outline" size="lg">
+          Button Primary
+        </Button>
+        <Button variant="outline" size="xl">
+          Button Primary
+        </Button>
+      </div>
+      <div className="flex gap-x-4">
+        <Button variant="link" size="icon">
+          <PlusIcon />
+        </Button>
+        <Button variant="link" size="sm">
+          Button Primary
+        </Button>
+        <Button variant="link">Button Primary</Button>
+        <Button variant="link" size="lg">
+          Button Primary
+        </Button>
+        <Button variant="link" size="xl">
+          Button Primary
+        </Button>
+      </div>
     </div>
   )
 }
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,58 @@
+import * as React from "react"
+
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        primary: "bg-primary text-white shadow hover:bg-primary/90",
+        destructive: "bg-error text-white shadow-sm hover:bg-error/90",
+        outline:
+          "border border-input bg-transparent shadow-sm hover:bg-grey-2 hover:text-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-secondary hover:text-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 px-3 text-xs",
+        lg: "h-10 px-8",
+        xl: "h-14 px-8 text-base",
+        icon: "size-9",
+        "icon-sm": "size-7",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+    },
+  },
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
