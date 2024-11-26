bc8aafbea0a1590cafd8701798baf2ec4b6850fd
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-11-26 07:29:29 +0700

    Setup hono

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
+const routes = app
+
+export const GET = handle(app)
+export const POST = handle(app)
+export const PATCH = handle(app)
+export const DELETE = handle(app)
+
+export type AppType = typeof routes
