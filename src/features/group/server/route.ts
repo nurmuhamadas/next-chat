0bcb5fec8aeb374fd2116f228900c1d2d3d84de6
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-12-02 10:14:57 +0700

    Rearrange method

--- src/features/group/server/route.ts
+++ src/features/group/server/route.ts
@@ -67,6 +67,88 @@ const groupApp = new Hono()
       return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
     }
   })
+  .post(
+    "/",
+    sessionMiddleware,
+    validateProfileMiddleware,
+    zValidator("form", groupSchema, zodErrorHandler),
+    async (c) => {
+      try {
+        const { name, image, type, memberIds, description } =
+          c.req.valid("form")
+        const imageFile = image as unknown as File
+
+        const databases = c.get("databases")
+        const storage = c.get("storage")
+        const currentProfile = c.get("userProfile")
+
+        const invalid = await validateGroupData(databases, currentProfile.$id, {
+          name,
+          description,
+          type,
+          memberIds,
+        })
+        if (invalid) {
+          return c.json(createError(invalid.error, invalid.path), invalid.code)
+        }
+
+        let imageUrl: string | undefined
+        let fileId: string | undefined
+        if (imageFile) {
+          const file = await uploadImage(storage, { image: imageFile })
+          fileId = file.$id
+          imageUrl = constructFileUrl(file.$id)
+        }
+
+        try {
+          const inviteCode = await createGroupInviteCode(databases)
+
+          const createdGroup = await createGroup(databases, {
+            name,
+            description,
+            type,
+            imageUrl,
+            ownerId: currentProfile.$id,
+            inviteCode,
+          })
+          await createGroupOption(databases, {
+            groupId: createdGroup.$id,
+            notification: true,
+          })
+          await createGroupMember(databases, {
+            userId: currentProfile.$id,
+            groupId: createdGroup.$id,
+            isAdmin: true,
+            joinedAt: new Date(),
+          })
+
+          await Promise.all([
+            ...memberIds.map((memberId) =>
+              createGroupMember(databases, {
+                userId: memberId,
+                groupId: createdGroup.$id,
+                isAdmin: false,
+                joinedAt: new Date(),
+              }),
+            ),
+          ])
+          const groupResult = mapGroupModelToGroup(
+            createdGroup,
+            mapUserModelToGroupOwner(currentProfile),
+          )
+          const response: CreateGroupResponse = successResponse(groupResult)
+          return c.json(response)
+        } catch {
+          if (fileId) {
+            await deleteImage(storage, { id: fileId })
+          }
+          return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
+        }
+      } catch {
+        return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
+      }
+    },
+  )
   .get(
     "/search",
     sessionMiddleware,
@@ -163,88 +245,6 @@ const groupApp = new Hono()
           result.total,
         )
         return c.json(response)
-      } catch {
-        return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
-      }
-    },
-  )
-  .post(
-    "/",
-    sessionMiddleware,
-    validateProfileMiddleware,
-    zValidator("form", groupSchema, zodErrorHandler),
-    async (c) => {
-      try {
-        const { name, image, type, memberIds, description } =
-          c.req.valid("form")
-        const imageFile = image as unknown as File
-
-        const databases = c.get("databases")
-        const storage = c.get("storage")
-        const currentProfile = c.get("userProfile")
-
-        const invalid = await validateGroupData(databases, currentProfile.$id, {
-          name,
-          description,
-          type,
-          memberIds,
-        })
-        if (invalid) {
-          return c.json(createError(invalid.error, invalid.path), invalid.code)
-        }
-
-        let imageUrl: string | undefined
-        let fileId: string | undefined
-        if (imageFile) {
-          const file = await uploadImage(storage, { image: imageFile })
-          fileId = file.$id
-          imageUrl = constructFileUrl(file.$id)
-        }
-
-        try {
-          const inviteCode = await createGroupInviteCode(databases)
-
-          const createdGroup = await createGroup(databases, {
-            name,
-            description,
-            type,
-            imageUrl,
-            ownerId: currentProfile.$id,
-            inviteCode,
-          })
-          await createGroupOption(databases, {
-            groupId: createdGroup.$id,
-            notification: true,
-          })
-          await createGroupMember(databases, {
-            userId: currentProfile.$id,
-            groupId: createdGroup.$id,
-            isAdmin: true,
-            joinedAt: new Date(),
-          })
-
-          await Promise.all([
-            ...memberIds.map((memberId) =>
-              createGroupMember(databases, {
-                userId: memberId,
-                groupId: createdGroup.$id,
-                isAdmin: false,
-                joinedAt: new Date(),
-              }),
-            ),
-          ])
-          const groupResult = mapGroupModelToGroup(
-            createdGroup,
-            mapUserModelToGroupOwner(currentProfile),
-          )
-          const response: CreateGroupResponse = successResponse(groupResult)
-          return c.json(response)
-        } catch {
-          if (fileId) {
-            await deleteImage(storage, { id: fileId })
-          }
-          return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
-        }
       } catch {
         return c.json(createError(ERROR.INTERNAL_SERVER_ERROR), 500)
       }
