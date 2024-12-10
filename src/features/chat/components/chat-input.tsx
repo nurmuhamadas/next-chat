29dcdca2e64731155ff1ed803dc5f276c3de3b77
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-12-10 07:06:25 +0700

    UI: Input file preview

--- src/features/chat/components/chat-input.tsx
+++ src/features/chat/components/chat-input.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { ChangeEventHandler, useRef, useState } from "react"
 
 import {
   ImageIcon,
@@ -14,11 +14,13 @@ import {
 
 import EmojiPopover from "@/components/emoji-popover"
 import { Button } from "@/components/ui/button"
+import useSendMessage from "@/features/messages/hooks/api/use-send-message"
 import { cn } from "@/lib/utils"
 
 import { useEditedMessageId } from "../hooks/use-edited-message-id"
 import { useRepliedMessageId } from "../hooks/use-replied-message-id"
 
+import InputFilePreview from "./input-file-preview"
 import TextEditor from "./text-editor"
 
 interface ChatInputProps {
@@ -35,15 +37,53 @@ interface ChatInputProps {
 }
 
 const ChatInput = ({ repliedMessage, editedMessage }: ChatInputProps) => {
+  const inputImageRef = useRef<HTMLInputElement>(null)
+  const inputFileRef = useRef<HTMLInputElement>(null)
+
   const { cancelReplyMessage } = useRepliedMessageId()
   const { cancelEditMessage } = useEditedMessageId()
 
   const [message, setMessage] = useState("")
+  const [attachments, setAttachments] = useState<File[]>([])
+
+  const { mutate: sendMessage } = useSendMessage()
+
+  const handleSend = () => {
+    sendMessage({
+      form: {},
+    })
+  }
+
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files)
+      setAttachments((prevAttachments) => [...prevAttachments, ...filesArray])
+    }
+  }
+
+  const handleRemoveFile = (index: number) => {
+    setAttachments((prevAttachments) =>
+      prevAttachments.filter((_, i) => i !== index),
+    )
+  }
 
   return (
     <div className="flex w-full justify-center p-4 pt-3">
       <div className="flex w-full max-w-[700px] items-end gap-x-2">
         <div className="flex flex-1 flex-col rounded-xl bg-surface p-3">
+          {attachments.length > 0 && (
+            <div className="mb-3 flex flex-wrap gap-2">
+              {attachments.map((file, i) => {
+                return (
+                  <InputFilePreview
+                    key={file.size + file.name}
+                    file={file}
+                    onRemove={() => handleRemoveFile(i)}
+                  />
+                )
+              })}
+            </div>
+          )}
           {editedMessage && (
             <div className={cn("mb-3 flex items-center gap-x-1")}>
               <PencilIcon className="mr-2 size-6 text-muted-foreground" />
@@ -100,29 +140,49 @@ const ChatInput = ({ repliedMessage, editedMessage }: ChatInputProps) => {
             <TextEditor onValueChange={setMessage} />
 
             <div className="gap-x-2 flex-center-end">
+              <input
+                type="file"
+                hidden
+                ref={inputImageRef}
+                accept="image/*"
+                className="hidden"
+                onChange={handleFileChange}
+                multiple
+              />
               <Button
                 variant="icon"
                 size="icon"
                 className="size-5 p-0 hover:text-primary"
+                onClick={() => inputImageRef.current?.click()}
               >
                 <ImageIcon />
               </Button>
+              <input
+                type="file"
+                hidden
+                ref={inputFileRef}
+                accept="*"
+                className="hidden"
+                multiple
+                onChange={handleFileChange}
+              />
               <Button
                 variant="icon"
                 size="icon"
                 className="size-5 p-0 hover:text-primary"
+                onClick={() => inputFileRef.current?.click()}
               >
                 <PaperclipIcon />
               </Button>
             </div>
           </div>
         </div>
-
         <div className="">
           {message.length > 0 ? (
             <Button
               className="size-11 bg-surface hover:bg-primary"
               variant="secondary"
+              onClick={handleSend}
             >
               <SendHorizonalIcon />
             </Button>
---
+++ src/features/chat/components/input-file-preview.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image"
+
+import { XIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+interface InputFilePreviewProps {
+  file: File
+  onRemove(): void
+}
+
+const InputFilePreview = ({ file, onRemove }: InputFilePreviewProps) => {
+  let url = "/images/folder.png"
+
+  if (file.type.startsWith("image/")) {
+    url = URL.createObjectURL(file)
+  } else if (file.type.startsWith("audio/")) {
+    url = "/images/audio.png"
+  } else if (file.type.startsWith("video/")) {
+    url = "/images/video.png"
+  } else if (file.type === "application/pdf") {
+    url = "/images/pdf.png"
+  } else if (
+    file.type ===
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  ) {
+    url = "/images/doc.png"
+  } else if (
+    file.type ===
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  ) {
+    url = "/images/xls.png"
+  }
+
+  return (
+    <div className="relative flex h-max w-[100px] flex-col overflow-hidden rounded-md bg-grey-4 p-1.5">
+      <Image
+        src={url}
+        alt={file.name}
+        width={75}
+        height={75}
+        className="size-full"
+      />
+      <p className="line-clamp-1 caption">{file.name}</p>
+      <Button
+        variant="icon"
+        size="icon-sm"
+        className="absolute right-0 top-0 size-6  bg-foreground/50 hover:bg-foreground"
+        onClick={onRemove}
+      >
+        <XIcon className="!size-3.5 text-surface" />
+      </Button>
+    </div>
+  )
+}
+
+export default InputFilePreview
