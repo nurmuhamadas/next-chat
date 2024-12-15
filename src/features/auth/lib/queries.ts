53158ac9ac1f1ca8512adcc1119bd79d10a1c819
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-12-16 06:27:29 +0700

    REFACTOR: reset all apis

--- src/features/auth/lib/queries.ts
+++ src/features/auth/lib/queries.ts
@@ -1,16 +1,8 @@
 import "server-only"
 
-import { getUserByEmail } from "@/features/user/lib/queries"
-import { createSessionClient } from "@/lib/appwrite"
-
 export const validateAuth = async () => {
   try {
-    const { account, databases } = await createSessionClient()
-    const user = await account.get()
-
-    const userProfile = await getUserByEmail(databases, { email: user.email })
-
-    return { isLoggedIn: !!user, isProfileCompleted: !!userProfile }
+    return { isLoggedIn: false, isProfileCompleted: false }
   } catch {
     return { isLoggedIn: false, isProfileCompleted: false }
   }
--- src/features/messages/lib/utils.ts
+++ src/features/messages/lib/utils.ts
@@ -1,5 +1,3 @@
-import { mergeName } from "@/lib/utils"
-
 export const getFileType = (mimeType: string): AttachmentType => {
   switch (mimeType) {
     case "image/jpeg":
@@ -23,72 +21,5 @@ export const getFileType = (mimeType: string): AttachmentType => {
 
     default:
       return "OTHER"
-  }
-}
-
-export const mapMessageModelToMessage = (
-  message: MessageAWModel,
-  user: UserAWModel,
-  attachments: Attachment[],
-  isRead: boolean,
-): Message => {
-  let fixedMessage = message.message
-  let fixedAttachments = attachments
-  if (message.status === "DELETED_BY_ADMIN") {
-    fixedMessage = "Message deleted by admin"
-    fixedAttachments = []
-  } else if (message.status === "DELETED_FOR_ALL") {
-    fixedAttachments = []
-    fixedMessage = "Message deleted"
-  } else if (
-    message.status === "DELETED_FOR_ME" &&
-    message.userId === user.$id
-  ) {
-    fixedAttachments = []
-    fixedMessage = ""
-  }
-
-  return {
-    id: message.$id,
-    user: mapUserModelToMessageAuthor(user),
-    message: fixedMessage ?? null,
-    conversationId: message.conversationId ?? null,
-    groupId: message.groupId ?? null,
-    channelId: message.channelId ?? null,
-    parentMessageId: message.parentMessageId ?? null,
-    parentMessageName: message.parentMessageName ?? null,
-    parentMessageText: message.parentMessageText ?? null,
-    originalMessageId: message.originalMessageId ?? null,
-    isEmojiOnly: message.isEmojiOnly,
-    status: message.status,
-    isRead,
-    attachments: fixedAttachments,
-    updatedAt: message.updatedAt ?? null,
-    createdAt: message.$createdAt,
-  }
-}
-
-export const mapUserModelToMessageAuthor = (
-  user: UserAWModel,
-): MessageAuthor => {
-  return {
-    id: user.$id,
-    name: mergeName(user.firstName, user.lastName),
-    imageUrl: user.imageUrl ?? null,
-  }
-}
-
-export const mapAttachmentModelToAttachment = (
-  attachment: AttachmentAWModel,
-  messageId: string,
-): Attachment => {
-  return {
-    id: attachment.$id,
-    messageId,
-    name: attachment.name,
-    size: attachment.size,
-    type: attachment.type,
-    url: attachment.url,
-    downloadUrl: attachment.downloadUrl,
   }
 }
