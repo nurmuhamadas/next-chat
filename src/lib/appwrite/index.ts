af61d9dbbf478903e94c32fa700a104a336c1042
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-12-16 17:49:32 +0700

    FIX: appwrite config

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,11 +1,6 @@
 import "server-only"
 
-import { cookies } from "next/headers"
-
-import { Account, Client, Databases, Storage, Users } from "node-appwrite"
-
-import { ERROR } from "@/constants/error"
-import { AUTH_COOKIE_KEY } from "@/features/auth/constants"
+import { Client, Storage } from "node-appwrite"
 
 import {
   APPWRITE_ENDPOINT,
@@ -21,38 +16,6 @@ export async function createAdminClient() {
     .setKey(APPWRITE_SECRET_KEY)
 
   return {
-    get account() {
-      return new Account(client)
-    },
-    get databases() {
-      return new Databases(client)
-    },
-    get users() {
-      return new Users(client)
-    },
-  }
-}
-
-export const createSessionClient = async () => {
-  const client = new Client()
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID)
-
-  const session = (await cookies()).get(AUTH_COOKIE_KEY)
-
-  if (!session) {
-    throw new Error(ERROR.UNAUTHORIZE)
-  }
-
-  client.setSession(session.value)
-
-  return {
-    get account() {
-      return new Account(client)
-    },
-    get databases() {
-      return new Databases(client)
-    },
     get storage() {
       return new Storage(client)
     },
