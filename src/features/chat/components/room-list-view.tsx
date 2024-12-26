40622ae9ca231441b6b4fc3cbc750568ae4a7954
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2024-12-27 05:43:22 +0700

    FIX: room list data

--- src/features/chat/components/room-list-view.tsx
+++ src/features/chat/components/room-list-view.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import Image from "next/image"
+
+import { LoaderIcon } from "lucide-react"
 
 import ChatSkeleton from "@/components/chat-skeleton"
 import { ScrollArea } from "@/components/ui/scroll-area"
@@ -12,11 +14,40 @@ import RoomListItem from "./room-list-item"
 
 const RoomListView = () => {
   const [cursor] = useState<string | undefined>(undefined)
+  const [rooms, setRooms] = useState<Room[]>([])
 
   const { data: settings, isLoading: settingLoading } = useGetSetting()
-  const { data: rooms, isLoading } = useGetRooms({ cursor })
+  const { data: roomsResult, isFetching: loadingRooms } = useGetRooms({
+    cursor,
+  })
 
-  if (isLoading || settingLoading) {
+  const isLoading = loadingRooms || settingLoading
+
+  useEffect(() => {
+    if (!loadingRooms && roomsResult.length > 0) {
+      setRooms((v) => {
+        const newRooms = [
+          ...v.filter((room) => !roomsResult.some((res) => res.id === room.id)),
+          ...roomsResult,
+        ].sort((a, b) => {
+          if (b.lastMessage && a.lastMessage) {
+            return b.lastMessage.id.localeCompare(a.lastMessage.id)
+          }
+          if (b.lastMessage) {
+            return b.lastMessage.id.localeCompare(a.id)
+          }
+          if (a.lastMessage) {
+            return b.id.localeCompare(a.lastMessage.id)
+          }
+          return b.id.localeCompare(a.id)
+        })
+
+        return newRooms
+      })
+    }
+  }, [loadingRooms])
+
+  if (isLoading && rooms.length === 0) {
     return <ChatSkeleton rows={5} />
   }
 
@@ -40,6 +71,7 @@ const RoomListView = () => {
       </div>
     )
   }
+
   return (
     <ScrollArea className="chat-list-scroll-area">
       <ul className="flex min-w-10 flex-col px-1.5 pt-2">
@@ -52,6 +84,12 @@ const RoomListView = () => {
             />
           )
         })}
+
+        {loadingRooms && rooms.length > 0 && (
+          <div className="h-24 flex-center">
+            <LoaderIcon className="size-5 animate-spin" />
+          </div>
+        )}
       </ul>
     </ScrollArea>
   )
--- src/features/chat/hooks/api/use-get-rooms.ts
+++ src/features/chat/hooks/api/use-get-rooms.ts
@@ -2,11 +2,17 @@ import { useQuery } from "@tanstack/react-query"
 
 import { client } from "@/lib/rpc"
 
-const useGetRooms = ({ cursor }: { cursor?: string }) => {
+const useGetRooms = ({
+  cursor,
+  limit,
+}: {
+  limit?: string
+  cursor?: string
+}) => {
   const query = useQuery({
     queryKey: ["rooms", cursor],
     queryFn: async () => {
-      const response = await client.api.rooms.$get({ query: { cursor } })
+      const response = await client.api.rooms.$get({ query: { cursor, limit } })
 
       const result = await response.json()
       if (!result.success) {
