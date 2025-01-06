ab42bbf87132b25d838f651c39c6a635a6128e3d
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-06 13:10:30 +0700

    CONFIG: add axios client for API requests and configure base URL

---
+++ src/lib/api/axios.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios, { AxiosError, AxiosResponse } from "axios"
+
+import { BASE_API_URL } from "../config"
+
+const apiClient = axios.create({
+  baseURL: BASE_API_URL,
+  timeout: 30000,
+})
+
+apiClient.interceptors.response.use(
+  (response): AxiosResponse<ApiResponse<any> | ApiCollectionResponse<any>> => {
+    const data = response.data
+    if (data.success) {
+      return response.data
+    } else {
+      throw new Error(data.error.message)
+    }
+  },
+  (error: AxiosError) => {
+    const data = error.response?.data as ErrorResponse
+    return Promise.reject(data.error)
+  },
+)
+
+export default apiClient
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,3 @@
 export const AUTH_SECRET = process.env.AUTH_SECRET!
+
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
