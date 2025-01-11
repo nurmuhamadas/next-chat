ef7bedd80cc3862f5dfe9d15ca614c99c7d804bb
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-11 12:36:50 +0700

    FIX: disable interval fetching

--- src/features/chat/components/chat-room-messages.tsx
+++ src/features/chat/components/chat-room-messages.tsx
@@ -61,7 +61,6 @@ const ChatRoomMessages = ({
   const {
     data: messages,
     isLoading: loadingMessage,
-    refetch,
     hasNextPage,
     fetchNextPage,
   } = useGetMessages({ id, roomType: type })
@@ -129,16 +128,6 @@ const ChatRoomMessages = ({
       onDeletedMessageChange(messages?.find((m) => m.id === deletedMessageId))
     }
   }, [deletedMessageId, onDeletedMessageChange, isLoading, messagesStr])
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      refetch()
-    }, 5000)
-
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [refetch])
 
   useEffect(() => {
     if (messages.length > 0) {
